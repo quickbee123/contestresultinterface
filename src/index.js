21ac40ea9d1eee106947656de754aca61350fdbd
Quickbee <rohitrobin111@gmail.com>
2021-03-16 18:20:39 +0530

    second

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ window.addEventListener("load", async () => {
 
     
     var unix = Math.round(Date.now()/1000);
-    console.log(unix);
+    
    
     var result1 =[];
     while(1){
@@ -42,7 +42,7 @@ window.addEventListener("load", async () => {
 
     
 
-    console.log(JSON.stringify(result1));
+   
 
 
     for (var i = 0; i < result1.length; i++){
@@ -54,7 +54,7 @@ window.addEventListener("load", async () => {
         const result3 = await (await acc.runLocal("contestCountdown",{})).decoded.output;
         const result4 = await (await acc.runLocal("votingCountdown",{})).decoded.output;
 
-        console.log(JSON.stringify(result3));
+        
 
         $("#contest_result").html($("#contest_result").html()+'<div  class="card d-flex justify-content-center" style="width:100%;">            <div class="card-body">            <h5 class="card-title">'+  new TextDecoder().decode(hex2a(result2.title)) +'</h5>            <p class="card-text"></p>            <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample'+i+'" role="button" aria-expanded="false" aria-controls="collapseExample">                View Results            </a>            <div class="collapse" id="collapseExample'+i+'">                <div id="card'+i+'" class="card card-body m-3">                                </div>            </div>        </div>    </div>');
           
@@ -79,6 +79,20 @@ window.addEventListener("load", async () => {
             $("#card"+i+"").text("Contest not started");
         }
         else{
+            const result6 = await (await acc.runLocal("listContenders",{})).decoded.output;
+            
+            for(var j = 0; j < result6.ids.length; j++){
+                const result7 = await (await acc.runLocal("getStatsFor",{"id":result6.ids[j]})).decoded.output;
+                console.log(JSON.stringify(result7));
+                if(j==0){
+                    
+                    $("#card"+i).html('<table class="table"> <thead> <tr> <th scope="col">Address</th> <th scope="col">Average</th> <th scope="col">Accepted</th> <th scope="col">Abstained</th> <th scope="col">Rejected</th> </tr> </thead> <tbody></tbody></table>');
+                }
+                
+                    $("#card"+i+" tbody").html( $("#card"+i+" tbody").html()+'<tr><td data-title="'+result6.addresses[j]+'">'+result6.addresses[j].substr(0,10)+'</td><td>'+result7.avgHi+"."+result7.avgLo+'</td><td>'+result7.accepted+'</td><td>'+result7.abstained+'</td><td>'+result7.rejected+'</td></tr> ')
+                    
+                 
+            }
             
         }
 
